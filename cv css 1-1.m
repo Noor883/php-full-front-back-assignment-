1ac1527d27c6ab06a29cc277aec5fcfe9482b375
Noor883 <115366016+Noor883@users.noreply.github.com>
2022-10-20 13:23:44 +0300

    Add files via upload
    cv without @media wek3

---
+++ cv css 1-1.m
@@ -0,0 +1,85 @@
+<!DOCTYPE>
+<html lang="en"><head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+   <-- <link rel="stylesheet" href="./assets/css/styles.css">  -->
+    
+</head>
+
+<body>
+<header>
+    <title>  CV NOOR</title>
+    <h1> My CV </h1>
+    <nav id=" cvNav ">
+        <ul>
+            <li>
+                <a href="http://gmail.com" target="_blank">gmail Account</a>
+            </li>
+            <li>
+                <a href="http://.com"> gmail Account</a>
+            </li>
+            <li>
+                <a href="http://github.com">Github Account</a>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- Basic bio -->
+    <section>
+        <div id="imageContainer">
+            <img src="./assesst/css/image/image eey 3.jpg" alt="my personal photo" width="200" height="250">
+            <!-- h2 was wrapped in span to change the block behavior -->
+            <div id="BasicInfoWrapper">
+                <h2> NOOR RASHEED </h2>
+                <h2>Computer Scince</h2>
+            </div>
+        </div>
+
+
+
+    </section>
+</header>
+<hr>
+<main>
+    <!-- Bio -->
+    <section>
+        <h3>Bio<span id="bioSimiC">:</span></h3>
+        <p><strong>Full Name:</strong> NOOR Rasheed is.</p>
+        <p><strong>DOB:</strong> <time datetime="2013-02-23">21rd of July, 2013</time></p>
+        <p><strong>Place of Birth:</strong> Amman</p>
+    </section>
+    <!-- Education -->
+        <h3> Education </h3>
+        <ul>
+            <li><time datetime="2013">2013</time>: phyl University, BA.</li>
+            <li><time datetime="2008">2008</time>: Some School.</li>
+        </ul>
+    
+    <!-- Career -->
+    <section>
+        <h3>Working Experiece</h3>
+        <ul>
+            <li><time datetime="2009">2011</time>:Some company.</li>
+            <li><time datetime="2015">2015</time>: programmer.</li>
+        </ul>
+    </section>
+</main>
+<footer>
+    <!-- Contact Information -->
+    <section>
+        <address>
+            <a href="mailto:drnoor883@gmail.com">noor@gmail.com</a>
+            <a href="tel:+9627968787">+962796878</a>
+        </address>
+    </section>
+
+    <!-- Date of creation -->
+    <section>
+        <time datetime="2022-10-9">9 Oct 2022</time>
+    </section>
+</footer>
+
+
+
